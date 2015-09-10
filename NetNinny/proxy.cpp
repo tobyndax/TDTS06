5ed92348c21498bf2b160bead7d0cdc1b206ea60
Jens Edhammer <tobyndax@hotmail.com>
2015-09-10 16:17:55 +0200

    Moved Sa_handler to it's proper place.

--- NetNinny/proxy.cpp
+++ NetNinny/proxy.cpp
@@ -42,16 +42,17 @@ void Proxy::connectBrowser(){
 	}
 
 
+	sa.sa_handler = sigchld_handler; // reap all dead processes
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
 
 	while(1){
 
-		sa.sa_handler = sigchld_handler; // reap all dead processes
-		sigemptyset(&sa.sa_mask);
-		sa.sa_flags = SA_RESTART;
-		if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-			perror("sigaction");
-			exit(1);
-		}
 
 		sockaddr_in browAddr;
 		socklen_t browAddrLen;
